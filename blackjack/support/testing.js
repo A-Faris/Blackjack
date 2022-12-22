c2b3c597bdd21a71bb53f90374e0d88a76e74bae
Chris Owen <chris@thecodethatchriswrote.com>
2022-12-22 14:21:58 +0000

    Update testing.js

--- blackjack/support/testing.js
+++ blackjack/support/testing.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 import { generateDeck, playerTurn } from '../blackjack.js'
 import { stub } from 'https://deno.land/x/mock@0.13.0/stub.ts'
 import shuffle from './shuffle.js'
