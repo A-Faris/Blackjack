7e9e6d5110c213320dfb8414edb99c2591756a26
Dan Keefe <peritract@hotmail.co.uk>
2023-06-06 11:42:07 +0100

    Update testing_util.py
    Remove manual output capture

--- blackjack/support/testing_util.py
+++ blackjack/support/testing_util.py
@@ -1,9 +1,5 @@
 # pylint: skip-file
 
-import sys
-from io import StringIO
-
-
 def player_chooses(choices: list, monkeypatch) -> None:
     """
     Take a list of choices and uses them to feed into the game to test with
@@ -15,21 +11,3 @@ def player_chooses(choices: list, monkeypatch) -> None:
     """
     answers = iter(choices)
     monkeypatch.setattr('builtins.input', lambda name: next(answers))
-
-
-def reset_test_suite() -> None:
-    """
-    Reset the test suite, ready to run another test. 
-    Should be run at the end of every test
-    """
-    sys.stdout = sys.__stdout__
-
-
-def capture_print_lines() -> None:
-    """
-    Capture all of the lines print'ed by the user during the game so we
-    can test if they are correct
-    """
-    captured_output = StringIO()
-    sys.stdout = captured_output
-    return captured_output
