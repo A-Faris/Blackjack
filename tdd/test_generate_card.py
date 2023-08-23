6d86f86bf3555a0d468a6eb903b091e8f11ede3c
Chris Owen <chris.owen@sigmalabs.co.uk>
2023-08-23 15:22:29 +0100

    Add more challenges

---
+++ tdd/test_generate_card.py
@@ -0,0 +1,18 @@
+"""Tests for the generate_card.py file"""
+
+from generate_card import generate_card, get_score
+
+"""Test that the generate_card function returns the correct card"""
+# Write your test here
+
+"""Test that the get_score function returns the correct score"""
+# Write your test here
+
+"""Test that the generate_card function returns an error message if the suit is invalid"""
+# Write your test here
+
+"""Test that the generate_card function returns an error message if the number is invalid"""
+# Write your test here
+
+"""Test that the get_score function returns 0 if the card is invalid"""
+# Write your test here
