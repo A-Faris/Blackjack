b2a7b6ded846fc843833397ab785c5b0b390647f
Faris Abulula <abululb02@gmail.com>
2024-03-18 16:53:12 +0000

    added testes in tdd folder

--- tdd/test_generate_card.py
+++ tdd/test_generate_card.py
@@ -15,13 +15,29 @@ def test_generate_card():
 # TODO: Write several tests below to test this function
 
 
+def test_generate_card_suit_invalid():
+    assert generate_card("A", "5") == "Error"
+
+
 """Tests that the generate_card function returns an error message if the number is invalid"""
 # TODO: Write several tests below to test this function
+
+
+def test_generate_card_number_invalid():
+    assert generate_card("H", "27") == "Error"
 
 
 """Tests that the get_score function returns the correct score"""
 # TODO: Write several tests below to test this function
 
 
+def test_get_score():
+    assert get_score("8J") == "8"
+
+
 """Tests that the get_score function returns 0 if the card is invalid"""
 # TODO: Write several tests below to test this function
+
+
+def test_get_score_return_0():
+    assert get_score("27H") == 0
